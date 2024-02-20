d66e74ffd30c1953c2efe6fd0787b743a0465d5e
Aman-Raut <amanraut13@gmail.com>
2024-02-21 02:10:25 +0530

    modified:   multiServer.js 	new file:   public/error.html

--- multiServer.js
+++ multiServer.js
@@ -5,6 +5,20 @@ const path = require('path')
 const app = express();
 const publicPath = path.join(__dirname,'public')
 
-app.use(express.static(publicPath));
+// app.use(express.static(publicPath));
+app.get("/",(req,resp)=>{
+    resp.sendFile(`${publicPath}/home.html`)
+});
 
+app.get('/gulu',(req,resp)=>{
+    resp.sendFile(`${publicPath}/about.html`)
+})
+
+app.get('/help',(req,resp)=>{
+    resp.sendFile(`${publicPath}/help.html`)
+});
+
+app.get('*',(req,resp)=>{
+    resp.sendFile(`${publicPath}/error.html`)
+})
 app.listen(5000);
